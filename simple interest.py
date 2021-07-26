6732fd29255633bcf8eb7d46a972b9f57a01fd8b
EshaanSaha <85110444+EshaanSaha@users.noreply.github.com>
2021-07-26 16:38:46 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,8 @@
+P=float(input("Enter the Principal amount: "))
+R=float(input("Enter the Rate Of Interest: "))
+T=float(input("Enter the Time duration, in years: "))
+SI=P*R*T/100
+print(SI)
+X=P+SI
+print("The final value to be paid is:",X)
+
